36298cf23b5c45923bd6dbf617378a5840b726c6
Teja Akella <takella6315@gmail.com>
2025-02-02 15:40:25 -0500

    finished everything up till chapter 10. Done with login and signup.

--- movieStore/settings.py
+++ movieStore/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home'
+    'home',
+    'movies',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +128,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
       BASE_DIR / 'movieStore/static/',
   ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
