f8cc4c54fdfcdec02f1dd8c7490ae6b8408913be
Teja Akella <takella6315@gmail.com>
2025-02-13 18:03:32 -0500

    order and purchase page done

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('logout/', views.logout, name='accounts.logout'),
     path('forgot-password/', views.forgot_password, name='accounts.forgot_password'),
     path('reset-password/<str:username>/', views.reset_password, name='accounts.reset_password'),
+    path('orders/', views.orders, name='accounts.orders'),
+
 ]
